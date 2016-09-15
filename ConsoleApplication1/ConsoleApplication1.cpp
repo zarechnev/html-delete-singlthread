f3affaf20b6f1472586159e3ebdb89e1b76c2941
Ruslan A. Zarechnev <zra@corp.zdravalt.ru>
2016-09-15 10:54:57 +0700

    Введён один алгоритм.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,19 +1,23 @@
-// ConsoleApplication1.cpp : Defines the entry point for the console application.
-//
 #include "stdafx.h"
+#include <windows.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <windows.h>
-#include <time.h>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, char* argv[])
-{
+vector<string> arr;
+vector<string> ans;
+
+int main(int argc, char* argv[]) {
 	int start_time = clock();
 	ifstream html_file;
 	ofstream out_file;
+	string str_tmp;
 
 	if (argc > 1) {
 		html_file.open(argv[1]);
@@ -29,25 +33,34 @@ int main(int argc, char* argv[])
 		return -2;
 	}
 
-	string text = "";
-	char ch = '\0';
-	bool b = false;
-
-	while (html_file.peek() >= 0 && (ch = html_file.get())) {
-		if (ch == '>') b = true;
-		if (b) {
-			if (ch != '<' && ch != '>')
-				text += ch;
-		}
-		if (ch == '<') b = false;
-	}
-
 	out_file.open("out_singlethread.txt", ios::trunc);
 
-	out_file << text;
+	while (!html_file.eof()) {
+		getline(html_file, str_tmp);
+		arr.push_back(str_tmp);
+	}
 
-	out_file.close();
+	for (unsigned int k = 0; k < arr.size(); k++) {
+		string text = "";
+		char ch = '\0';
+		bool b = true;
+		for (unsigned int i = 0; i < arr[k].length(); i++) {
+			ch = arr[k][i];
+			if (ch == '<') b = false;
+			if (ch == '>') {
+				b = true;
+				continue;
+			}
+			if (b) text += ch;
+		}
+		ans.push_back(text);
+	}
+
+	for (unsigned int k = 0; k < ans.size(); k++)
+		out_file << ans[k] << endl;
+
 	html_file.close();
+	out_file.close();
 
 	int algorithm_end_time = clock();
 	cout << "Algorithm completed in: " << (algorithm_end_time - start_time) / 1000.0 << " seconds." << endl;
